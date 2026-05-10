0f6cd03a290189b41ba0ef7a0e66f302ca0248a7
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-05-10 23:01:05 +0600

    JS

--- find-out-get-strawberry.js
+++ find-out-get-strawberry.js
@@ -25,3 +25,33 @@ function willGet (num1, num2){
 
 const finalGet = willGet(30, 20);
 console.log("Congratulations, you are winner", finalGet);
+
+// Duplicate
+
+// Way 01
+
+const kabul = 65;
+const cabul = 70;
+
+if(kabul > cabul){
+    console.log("kabul will get the strawberry");
+}
+
+else{
+    console.log("cabul will get the strawberry");
+}
+
+// Way 02
+
+function willGet (num1, num2){
+    if (num1 > num2){
+        return num1
+    }
+
+    else{
+        return num2
+    }
+}
+
+const finalGet = willGet(30, 20);
+console.log("Congratulations, you are winner", finalGet);
